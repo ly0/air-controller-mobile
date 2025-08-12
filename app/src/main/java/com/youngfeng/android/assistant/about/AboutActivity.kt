ace618166aa8a9697b0f0ba137285440d9675a92
latyas <latyas@gmail.com>
2025-08-13 03:45:13 +0800

    add commit in about page

--- app/src/main/java/com/youngfeng/android/assistant/about/AboutActivity.kt
+++ app/src/main/java/com/youngfeng/android/assistant/about/AboutActivity.kt
@@ -8,11 +8,13 @@ import com.youngfeng.android.assistant.R
 
 class AboutActivity : AppCompatActivity() {
     private val mVersionText by lazy { findViewById<AppCompatTextView>(R.id.text_version) }
+    private val mCommitIdText by lazy { findViewById<AppCompatTextView>(R.id.text_commit_id) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
         mVersionText.text = BuildConfig.VERSION_NAME
+        mCommitIdText.text = "Commit: ${BuildConfig.GIT_COMMIT_ID}"
     }
 }
